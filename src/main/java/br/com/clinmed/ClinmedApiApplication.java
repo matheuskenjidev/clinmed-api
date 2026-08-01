463c9c478e94ea5eb776017a8d54f51eb51a9040
Matheus Kenji <matheuskenjidev@gmail.com>
2026-08-01 15:24:07 -0300

    primeiro commit inicial

---
+++ src/main/java/br/com/clinmed/ClinmedApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.clinmed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinmedApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinmedApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/clinmed/ClinmedApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.clinmed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinmedApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
