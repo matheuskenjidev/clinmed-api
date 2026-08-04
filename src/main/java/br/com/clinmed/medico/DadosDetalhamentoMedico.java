c1b8bd7f7eedd99cf14ebfebfb26894e830c2fef
Matheus Kenji <matheuskenjidev@gmail.com>
2026-08-04 14:40:10 -0300

    padroniza retornos da classe Controller de medicos

---
+++ src/main/java/br/com/clinmed/medico/DadosDetalhamentoMedico.java
@@ -0,0 +1,17 @@
+package br.com.clinmed.medico;
+
+import br.com.clinmed.endereco.Endereco;
+
+public record DadosDetalhamentoMedico(
+        Long id,
+        String nome,
+        String email,
+        String crm,
+        String telefone,
+        Especialidade especialidade,
+        Endereco endereco
+) {
+        public DadosDetalhamentoMedico(Medico medico) {
+            this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getTelefone(), medico.getEspecialidade(), medico.getEndereco());
+        }
+}
