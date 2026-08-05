6b70f08eb481cf2a27b21e9bacccd7899a12157f
Matheus Kenji <matheuskenjidev@gmail.com>
2026-08-05 18:20:03 -0300

    feat: implementa autorização e autenticação jwt

--- src/main/java/br/com/clinmed/infra/security/TokenService.java
+++ src/main/java/br/com/clinmed/infra/security/TokenService.java
@@ -2,8 +2,10 @@ package br.com.clinmed.infra.security;
 
 import br.com.clinmed.domain.usuario.Usuario;
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -29,6 +31,20 @@ public class TokenService {
         } catch (JWTCreationException exception){
             // Invalid Signing configuration / Couldn't convert Claims.
             throw new RuntimeException("erro ao gerar token jwt", exception);
+        }
+    }
+
+    public String getSubject(String tokenJWT) {
+        try {
+            Algorithm algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+                    .withIssuer("API Clinmed")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("Token JWT invalido ou expirado!");
         }
     }
 
